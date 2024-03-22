d51cf7afea7958b54aa5bdbc82041a219f9befe8
yvonnkr <yvonnkr86@gmail.com>
2024-03-22 21:04:29 +0000

    add job-service module

---
+++ job-service/src/test/java/com/yvolabs/jobservice/JobServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yvolabs.jobservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
