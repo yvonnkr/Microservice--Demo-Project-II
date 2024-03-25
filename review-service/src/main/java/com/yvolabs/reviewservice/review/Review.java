1c98d7de2e7c3caf7eadaf8214a0c85b9016d9b0
yvonnkr <yvonnkr86@gmail.com>
2024-03-25 23:47:23 +0000

    implement review-service module

---
+++ review-service/src/main/java/com/yvolabs/reviewservice/review/Review.java
@@ -0,0 +1,24 @@
+package com.yvolabs.reviewservice.review;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+    private double rating;
+    private Long companyId;
+
+}
