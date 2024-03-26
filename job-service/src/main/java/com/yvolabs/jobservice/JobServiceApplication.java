0fad2efca94c4203c3fa017f65081b7674e3cc04
yvonnkr <yvonnkr86@gmail.com>
2024-03-26 16:03:14 +0000

    register job-service with service-registry

--- job-service/src/main/java/com/yvolabs/jobservice/JobServiceApplication.java
+++ job-service/src/main/java/com/yvolabs/jobservice/JobServiceApplication.java
@@ -2,8 +2,10 @@ package com.yvolabs.jobservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class JobServiceApplication {
 
     public static void main(String[] args) {
